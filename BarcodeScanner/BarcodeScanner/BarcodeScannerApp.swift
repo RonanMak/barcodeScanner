15da7c7c5d8f928bc84b0a54c1c49b9446b23d66
Ronan <ronsmmak817@gmail.com>
2022-09-04 15:23:25 +0800

    first commit

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Ronan Mak on 4/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarcodeScanner
+//
+//  Created by Ronan Mak on 4/9/2022.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeScannerView()
+    }
+}
